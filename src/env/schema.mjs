21bdef1b022f12ad8912f0a9d0ebf8e26c7d96d3
leona-art <leonagraph@gmail.com>
2023-01-11 19:13:31 +0900

    fix

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,17 +7,17 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXTAUTH_SECRET:
-    process.env.NODE_ENV === "production"
-      ? z.string().min(1)
-      : z.string().min(1).optional(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
+  // NEXTAUTH_SECRET:
+  //   process.env.NODE_ENV === "production"
+  //     ? z.string().min(1)
+  //     : z.string().min(1).optional(),
+  // NEXTAUTH_URL: z.preprocess(
+  //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+  //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
+  //   (str) => process.env.VERCEL_URL ?? str,
+  //   // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+  //   process.env.VERCEL ? z.string() : z.string().url(),
+  // ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
   GOOGLE_CLIENT_ID:z.string(),
@@ -31,8 +31,8 @@ export const serverSchema = z.object({
  */
 export const serverEnv = {
   NODE_ENV: process.env.NODE_ENV,
-  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
   GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
